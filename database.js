8c4529ad82412328fd8f980fba3c3173ff1d08fb
almogshemesh15 <almogshemesh11@gmail.com>
2026-07-11 01:58:55 +0300

    Update database.js

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ async function loadData() {
     try {
         const res = await axios.get(GOOGLE_SHEET_URL);
         if (res.data) {
-            data = JSON.parse(JSON.stringify(res.data)); 
+            data = JSON.parse(JSON.stringify(res.data));
         }
     } catch (e) {
         console.error(e);
@@ -20,7 +20,28 @@ async function save() {
     } catch (e) {}
 }
 
+function checkExpiration() {
+    const now = Date.now();
+    let changed = false;
+
+    ['creators', 'places'].forEach(type => {
+        data.whitelist[type].forEach(item => {
+            if (item.keys && Array.isArray(item.keys)) {
+                const initialLength = item.keys.length;
+                item.keys = item.keys.filter(k => !k.expiresAt || k.expiresAt > now);
+                if (item.keys.length !== initialLength) changed = true;
+            }
+            if (item.expiresAt && item.expiresAt <= now) {
+                item.expiresAt = null;
+                changed = true;
+            }
+        });
+    });
+
+    if (changed) save();
+}
+
 setInterval(loadData, 30000);
 loadData();
 
-module.exports = { getData: () => data, save, checkExpiration: () => {} };
+module.exports = { getData: () => data, save, checkExpiration };
