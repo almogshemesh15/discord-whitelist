232e300fef0b3117997d0e650fd8bd15a449dcd8
almogshemesh15 <almogshemesh11@gmail.com>
2026-07-11 01:41:46 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,48 +1,24 @@
 const axios = require('axios');
-
 const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzlYXcPdq5BtPttfrHBC290DK6tzS69fdc95GKwDD8cSbsiZzmkD-rVogxuUeia0HeL/exec';
 
-let data = {
-    whitelist: { creators: [], places: [] },
-    pendingPlaces: [],
-    keys: []
-};
+let data = { whitelist: { creators: [], places: [] }, pendingPlaces: [], keys: [] };
 
 async function loadData() {
     try {
         const res = await axios.get(GOOGLE_SHEET_URL);
-        const rows = res.data;
-        data.whitelist = { creators: [], places: [] };
-        data.pendingPlaces = [];
-        data.keys = [];
-        rows.forEach((row, index) => {
-            if (index === 0) return;
-            const [name, id, key, type] = row;
-            if (type === 'creators' || type === 'places') {
-                data.whitelist[type].push({ id: Number(id), name, keys: [{ key }] });
-            } else if (type === 'key') {
-                data.keys.push({ key: id });
-            }
-        });
+        if (res.data && res.data[1]) {
+            data = JSON.parse(res.data[1][0]);
+        }
     } catch (e) {}
 }
 
 async function save() {
     try {
-        await axios.post(GOOGLE_SHEET_URL, {
-            action: 'update',
-            data: data
-        });
+        await axios.post(GOOGLE_SHEET_URL, { action: 'update', data: JSON.stringify(data) });
     } catch (e) {}
 }
 
 setInterval(loadData, 30000);
 loadData();
 
-function checkExpiration() {}
-
-module.exports = {
-    getData: () => data,
-    save,
-    checkExpiration
-};
+module.exports = { getData: () => data, save, checkExpiration: () => {} };
