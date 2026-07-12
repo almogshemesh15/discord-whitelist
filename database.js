332430fcdda3acb83d3554d7bf8d6d51d5b65564
almogshemesh15 <almogshemesh11@gmail.com>
2026-07-12 20:17:55 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,17 +1,15 @@
 const axios = require('axios');
 const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzlYXcPdq5BtPttfrHBC290DK6tzS69fdc95GKwDD8cSbsiZzmkD-rVogxuUeia0HeL/exec';
 
-let data = { whitelist: { creators: [], places: [] }, pendingPlaces: [], keys: [] };
+let data = { whitelist: { creators: [], places: [] }, pendingPlaces: [], logs: [], activeSessions: [], keys: [] };
 
 async function loadData() {
     try {
         const res = await axios.get(GOOGLE_SHEET_URL);
-        if (res.data) {
-            data = JSON.parse(JSON.stringify(res.data));
+        if (res.data && typeof res.data === 'object') {
+            data = res.data;
         }
-    } catch (e) {
-        console.error(e);
-    }
+    } catch (e) {}
 }
 
 async function save() {
@@ -41,7 +39,8 @@ function checkExpiration() {
     if (changed) save();
 }
 
-setInterval(loadData, 30000);
-loadData();
+loadData().then(() => {
+    setInterval(loadData, 30000);
+});
 
 module.exports = { getData: () => data, save, checkExpiration };
